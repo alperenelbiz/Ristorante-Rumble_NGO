38ec74ff6638d0b4a2702c0ad782a58768711eea
H. Alperen Elbiz <94225207+alperenelbiz@users.noreply.github.com>
2025-06-22 00:13:27 +0300

    Client and Server Rpc Test

--- Assets/Scripts/Player/PlayerNetwork.cs
+++ Assets/Scripts/Player/PlayerNetwork.cs
@@ -1,12 +1,59 @@
+using System.Collections.Generic;
 using System.Globalization;
+using NUnit.Framework;
+using Unity.Collections;
 using Unity.Netcode;
 using UnityEngine;
 
 public class PlayerNetwork : NetworkBehaviour
 {
+    // client cann write into the server by this method
+    private NetworkVariable<MyCustomData> randomNumber = new NetworkVariable<MyCustomData>(
+        new MyCustomData
+        {
+            _int = 56,
+            _bool = true,
+        }, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Owner)
+    {
+    };
+
+    public struct MyCustomData : INetworkSerializable
+    {
+        public int _int;
+        public bool _bool;
+        public FixedString128Bytes message;
+
+        public void NetworkSerialize<T>(BufferSerializer<T> serializer) where T : IReaderWriter
+        {
+            serializer.SerializeValue(ref _int);
+            serializer.SerializeValue(ref _bool);
+            serializer.SerializeValue(ref message);
+        }
+    }
+
+    public override void OnNetworkSpawn()
+    {
+        randomNumber.OnValueChanged += (MyCustomData previousValue, MyCustomData newValue) =>
+        {
+            Debug.Log(OwnerClientId + "; randomNumber: " + newValue._int + "; " + newValue._bool + "; " + newValue.message);
+        };
+    }
+
     private void Update()
     {
-        if (!IsOwner) return;
+        if (!IsOwner) return; 
+
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            //TestServerRpc(new ServerRpcParams());
+            TestClientRpc(new ClientRpcParams { Send = new ClientRpcSendParams { TargetClientIds = new List<ulong> { 1 /*sends the message to specific client 1*/ } } });
+            /*randomNumber.Value = new MyCustomData
+            {
+                _int = Random.Range(0, 100),
+                _bool = false,
+                message = "All your base are belong to us!"
+            };*/
+        }
 
         Vector3 moveDir = new Vector3(0, 0, 0);
 
@@ -17,5 +64,17 @@ public class PlayerNetwork : NetworkBehaviour
 
         float moveSpeed = 3f;
         transform.position += moveDir * moveSpeed * Time.deltaTime;
+    }
+
+    [ServerRpc] //send message from server to server
+    private void TestServerRpc(ServerRpcParams serverRpcParams)
+    {
+        Debug.Log("TestServerRpc " + OwnerClientId + "; " + serverRpcParams.Receive.SenderClientId);
+    }
+
+    [ClientRpc] //send message from server to clients
+    private void TestClientRpc(ClientRpcParams clientRpcParams)
+    {
+        Debug.Log("TestClientRpc");
     }
 }
